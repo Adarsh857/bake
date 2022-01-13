74c006e7286c86e859b04d647485d7f99af6fc5c
adarsh sharma <sharma2015adarsh@gmail.com>
2022-01-13 16:26:59 +0530

    deploy

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -16,12 +16,12 @@ const Sidebar = ({ isOpen, toggle }) => {
         <CloseIcon />
       </Icon>
       <SidebarMenu>
-        <SidebarLink to='/'>Pizzas</SidebarLink>
-        <SidebarLink to='/'>Desserts</SidebarLink>
-        <SidebarLink to='/'>Full Menu</SidebarLink>
+        <SidebarLink to='/'>Pizza</SidebarLink>
+        <SidebarLink to='/'>Dessert</SidebarLink>
+        <SidebarLink to='/'> Menu</SidebarLink>
       </SidebarMenu>
       <SideBtnWrap>
-        <SidebarRoute to='/'>Order Now</SidebarRoute>
+        <SidebarRoute to='/'>Order Right Now</SidebarRoute>
       </SideBtnWrap>
     </SidebarContainer>
   );
