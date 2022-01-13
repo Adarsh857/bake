d76fb6eeb6bcfd085a4cea50af2cd597cd4e1e19
adarsh sharma <sharma2015adarsh@gmail.com>
2022-01-13 16:30:12 +0530

    deployed

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,7 +5,7 @@ const Navbar = ({ toggle }) => {
   return (
     <>
       <Nav>
-        <NavLink to='/'>BakeIt</NavLink>
+        <NavLink to='/'>BakeIT</NavLink>
         <NavIcon onClick={toggle}>
           <p>Menu</p>
           <Bars />
